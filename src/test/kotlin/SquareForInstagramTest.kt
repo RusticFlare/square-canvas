80653330f7e7eefca28112624b89347cc7aea97a
James Baker <j_d_baker@hotmail.com>
2020-09-10 14:37:32 +0100

    Rename and refactor

--- src/test/kotlin/SquareForInstagramTest.kt
+++ src/test/kotlin/SquareForInstagramTest.kt
@@ -12,7 +12,7 @@ import java.net.URL
 import javax.imageio.ImageIO
 import kotlin.math.abs
 
-class SquareCanvasTest : FunSpec({
+class SquareForInstagramTest : FunSpec({
 
     context("Test squareForInstagram") {
         forAll(
@@ -38,31 +38,23 @@ private infix fun File.differenceTo(image: File) = difference(ImageIO.read(this)
 
 private fun difference(image1: BufferedImage, image2: BufferedImage): Double {
     val w1 = image1.width
-    val w2 = image2.width
     val h1 = image1.height
+    val w2 = image2.width
     val h2 = image2.height
 
     check(w1 == w2 && h1 == h2) { "Both images should have same dimensions h1=$h1 w1=$w1 h2=$h2 w2=$w2" }
 
-    var diff: Long = 0
-    for (j in 0 until h1) {
-        for (i in 0 until w1) {
-            //Getting the RGB values of a pixel
-            val pixel1 = image1.getRGB(i, j)
-            val color1 = Color(pixel1, true)
-            val r1: Int = color1.red
-            val g1: Int = color1.green
-            val b1: Int = color1.blue
-            val pixel2 = image2.getRGB(i, j)
-            val color2 = Color(pixel2, true)
-            val r2: Int = color2.red
-            val g2: Int = color2.green
-            val b2: Int = color2.blue
-            //sum of differences of RGB values of the two images
-            val data = abs(r1 - r2) + abs(g1 - g2) + abs(b1 - b2).toLong()
-            diff += data
-        }
-    }
+    val diff = (0 until w1).crossProduct(0 until h1)
+        .sumBy { (x, y) -> image1.color(x, y) - image2.color(x, y) }
+
     val avg = diff / (w1 * h1 * 3).toDouble()
     return avg / 255 * 100
 }
+
+private fun IntRange.crossProduct(other: IntRange) =
+    asSequence().flatMap { first -> other.asSequence().map { second -> first to second } }
+
+private fun BufferedImage.color(x: Int, y: Int) = Color(getRGB(x, y), true)
+
+private operator fun Color.minus(other: Color) =
+    abs(this.red - other.red) + abs(this.green - other.green) + abs(this.blue - other.blue)
